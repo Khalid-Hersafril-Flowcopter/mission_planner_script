c834c34e546123ff5254b413c98aefcda28f3396
Khalid-Hersafril-Flowcopter <khalid.hersafril@flowcopter.com>
2023-04-06 14:38:04 +0100

    Working version of trigger

--- rc8_trigger.lua
+++ rc8_trigger.lua
@@ -10,25 +10,26 @@ function update() -- this is the loop which periodically runs
     gcs:send_text(6, 'LUA: get SCR_ENABLE failed')
   end
 
-  local RC8_trigger = param:get('RC8_OPTION')
+  local chan_8_pwm = rc:get_pwm(7)
   
+  pwm_threshold = 1900
   RCIN9 = 59
   RCIN10 = 60
   MOTOR1 = 33
   MOTOR2 = 34
 
-  if RC8_trigger == 300 then
+  if chan_8_pwm < pwm_threshold then
     gcs:send_text(6, string.format('LUA: SERVO1_FUNCTION set to %i', RCIN9))
     gcs:send_text(6, string.format('LUA: SERVO2_FUNCTION set to %i', RCIN10))
     
-    param:set('SERVO1_FUNCTION', RCIN9)
-    param:set('SERVO2_FUNCTION', RCIN10)
+    param:set_and_save('SERVO1_FUNCTION', RCIN9)
+    param:set_and_save('SERVO2_FUNCTION', RCIN10)
   else 
     gcs:send_text(6, string.format('LUA: SERVO1_FUNCTION set to default value - %i', MOTOR1))
     gcs:send_text(6, string.format('LUA: SERVO2_FUNCTION set to default value - %i', MOTOR2))
-    param:set('SERVO1_FUNCTION', MOTOR1)
-    param:set('SERVO2_FUNCTION', MOTOR2)
+    param:set_and_save('SERVO1_FUNCTION', MOTOR1)
+    param:set_and_save('SERVO2_FUNCTION', MOTOR2)
   end
-  return update, 1000 -- reschedules the loop
+  return update, 100 -- reschedules the loop
 end
-return update, 1000 -- run immediately before starting to reschedule
+return update, 100 -- run immediately before starting to reschedule
