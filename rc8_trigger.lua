ba8bed0575a105c372be5f8beff06e7a9a0bd801
Khalid-Hersafril-Flowcopter <khalid.hersafril@flowcopter.com>
2023-04-07 11:10:04 +0100

    Change conditions for rc8 trigger

--- rc8_trigger.lua
+++ rc8_trigger.lua
@@ -1,8 +1,19 @@
+local init_flag
+local end_flag
+
+pwm_threshold = 1900
+RCIN9 = 59
+RCIN10 = 60
+MOTOR1 = 33
+MOTOR2 = 34
+
+
+gcs:send_text(6, 'LUA: RC8 Trigger script loaded')
+local value = param:get('SCR_ENABLE')
+
 function update() -- this is the loop which periodically runs
-  
-   gcs:send_text(6, 'LUA: RC8 Trigger script loaded')
-  -- get and print all the scripting parameters
-  local value = param:get('SCR_ENABLE')
+ 
+  init_flag = param:get('SERVO1_FUNCTION')
 
   if value then
     gcs:send_text(6, string.format('LUA: SCR_ENABLE: %i',value))
@@ -10,26 +21,25 @@ function update() -- this is the loop which periodically runs
     gcs:send_text(6, 'LUA: get SCR_ENABLE failed')
   end
 
-  local chan_8_pwm = rc:get_pwm(7)
+  local chan_7_pwm = rc:get_pwm(7)
   
-  pwm_threshold = 1900
-  RCIN9 = 59
-  RCIN10 = 60
-  MOTOR1 = 33
-  MOTOR2 = 34
+  if chan_7_pwm < pwm_threshold then
 
-  if chan_8_pwm < pwm_threshold then
-    gcs:send_text(6, string.format('LUA: SERVO1_FUNCTION set to %i', RCIN9))
-    gcs:send_text(6, string.format('LUA: SERVO2_FUNCTION set to %i', RCIN10))
-    
-    param:set_and_save('SERVO1_FUNCTION', RCIN9)
-    param:set_and_save('SERVO2_FUNCTION', RCIN10)
+    if init_flag ~= end_flag then 
+      gcs:send_text(6, string.format('LUA: SERVO1_FUNCTION set to %i', RCIN9))
+      gcs:send_text(6, string.format('LUA: SERVO2_FUNCTION set to %i', RCIN10))
+    end
+   
+    param:set('SERVO1_FUNCTION', RCIN9)
+    param:set('SERVO2_FUNCTION', RCIN10)
   else 
     gcs:send_text(6, string.format('LUA: SERVO1_FUNCTION set to default value - %i', MOTOR1))
     gcs:send_text(6, string.format('LUA: SERVO2_FUNCTION set to default value - %i', MOTOR2))
-    param:set_and_save('SERVO1_FUNCTION', MOTOR1)
-    param:set_and_save('SERVO2_FUNCTION', MOTOR2)
+    param:set('SERVO1_FUNCTION', MOTOR1)
+    param:set('SERVO2_FUNCTION', MOTOR2)
   end
+
+  end_flag = param:get('SERVO1_FUNCTION')
   return update, 100 -- reschedules the loop
 end
 return update, 100 -- run immediately before starting to reschedule
