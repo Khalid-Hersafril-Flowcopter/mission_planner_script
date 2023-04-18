67c0d748242dcd59c5ce6ddabed02b1060c0313c
Khalid-Hersafril-Flowcopter <khalid.hersafril@flowcopter.com>
2023-04-18 10:58:59 +0100

    Ensure message panel is not flooded

--- rc8_trigger.lua
+++ rc8_trigger.lua
@@ -15,31 +15,23 @@ function update() -- this is the loop which periodically runs
  
   init_flag = param:get('SERVO1_FUNCTION')
 
-  if value then
-    gcs:send_text(6, string.format('LUA: SCR_ENABLE: %i',value))
-  elseif value == 0 then
-    gcs:send_text(6, 'LUA: get SCR_ENABLE failed')
-  end
-
   local chan_7_pwm = rc:get_pwm(7)
   
   if chan_7_pwm < pwm_threshold then
-
-    if init_flag ~= end_flag then 
-      gcs:send_text(6, string.format('LUA: SERVO1_FUNCTION set to %i', RCIN9))
-      gcs:send_text(6, string.format('LUA: SERVO2_FUNCTION set to %i', RCIN10))
-    end
-   
     param:set('SERVO1_FUNCTION', RCIN9)
     param:set('SERVO2_FUNCTION', RCIN10)
   else 
-    gcs:send_text(6, string.format('LUA: SERVO1_FUNCTION set to default value - %i', MOTOR1))
-    gcs:send_text(6, string.format('LUA: SERVO2_FUNCTION set to default value - %i', MOTOR2))
     param:set('SERVO1_FUNCTION', MOTOR1)
     param:set('SERVO2_FUNCTION', MOTOR2)
   end
-
   end_flag = param:get('SERVO1_FUNCTION')
+
+  -- Ensure that the message panel is not flooded
+  if init_flag  ~= end_flag then
+    gcs:send_text(6, string.format('LUA: SERVO1_FUNCTION set to %i', param:get('SERVO1_FUNCTION')))
+    gcs:send_text(6, string.format('LUA: SERVO2_FUNCTION set to %i', param:get('SERVO2_FUNCTION')))
+  end
+  
   return update, 100 -- reschedules the loop
 end
 return update, 100 -- run immediately before starting to reschedule
